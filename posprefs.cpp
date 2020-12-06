7fd2db4fd785115ab09afa2bc0b3e15587cfc878
bhavinisehgal18 <62087213+bhavinisehgal18@users.noreply.github.com>
2020-12-07 00:53:03 +0530

    Create posprefs.cpp

---
+++ posprefs.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+int main() 
+{   
+    int n, k;
+    cin>>n>>k;
+    int arr[n];
+    int sum=0;
+    int c=0;
+    for(int i=1; i<=n; i++)
+    {
+        arr[i]=i;
+    }
+    if(n%2==0 && k!=n)
+    {
+        for(int i=1; i<=n; i++)
+        {
+            if(i%2!=0)
+            {
+               arr[i]=(-i);
+            }
+        }
+    }
+    if(n%2!=0 && k!=n)
+    {
+        for(int i=1; i<=n; i++)
+        {
+            if(i%2==0)
+            {
+               arr[i]=(-i);
+            }
+        }
+    }
+    for(int i=1; i<=n; i++)
+    {
+        sum=sum+arr[i];
+        if(sum>0)
+        {
+            c++;
+        }
+    }
+                 
+        if(c>k)
+        {
+            for(int i=n; i>0; )
+            {
+                arr[i]= (-i);
+                c--;
+                i = i-2;
+                if(c==k)
+                {
+                    break;
+                }
+            }
+        }
+        if(c<k)
+        {
+           for(int i=n-1; i>0; )
+            {
+                arr[i]= (i);
+                c++;
+                i = i-2;
+                if(c==k)
+                {
+                    break;
+                }
+            }
+        }
+       
+
+    for(int i=1; i<=n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<c;
+    return 0;
+}
+
+
